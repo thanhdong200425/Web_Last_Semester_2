24125280bb34b0369d169e9ee3f3947248588c2d
Khang Phan <phanbaokhang0205@gmail.com>
2023-11-27 22:04:24 +0700

    update seek_bar

--- Client/js/homepage.js
+++ Client/js/homepage.js
@@ -1,6 +1,35 @@
 const btnplay = document.querySelector('.btn-play');
+const btnmute = document.querySelector('.bxs-volume-full');
+const iconVolume = document.getElementById('iconVolume');
+const inputVolume = document.getElementById('inputVolume');
 
+
+//Hieu ung play-pause
 btnplay.addEventListener('click', ()=> {
     btnplay.classList.toggle('pause');
     btnplay.classList.toggle('play');
-})
+});
+
+// Doi mau trai tim
+function changeColor() {
+    var heart = document.getElementById("heart");
+    heart.classList.toggle("clicked");
+}
+
+//Hieu ung mute
+btnmute.addEventListener('click', ()=> {
+    btnmute.classList.toggle('mute');
+    btnmute.classList.toggle('unmute');
+});
+
+// Hieu ung input volume
+var currentval = 0;
+iconVolume.addEventListener('click', ()=> {
+    if (currentval === 0) {
+        inputVolume.value = 100;
+        currentval = 100;
+    }else {
+        inputVolume.value = 0;
+        currentval = 0;
+    }
+});
