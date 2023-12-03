1c248512bed982afa9868cb2c3cda96d330bb7b6
Khang Phan <phanbaokhang0205@gmail.com>
2023-12-03 07:36:29 +0700

    update

---
+++ Client/js/songs.js
@@ -0,0 +1,14 @@
+let songs = [
+    {
+        name: 'Tại Vì Sao',
+        path: '../audios/taivisao-mck.mp3',
+        singers: 'MCK',
+        image: '../img/taivisao_mck.jpg'
+    },
+    {
+        name: 'Tại Vì Sao',
+        path: '../audios/taivisao-mck.mp3',
+        singers: 'MCK',
+        image: '../img/taivisao_mck.jpg'
+    }
+]
